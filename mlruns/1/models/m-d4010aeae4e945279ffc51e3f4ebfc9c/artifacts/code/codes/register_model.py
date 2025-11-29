b2b5d310bc402d5c14dd911127ae36da575e16c2
siddeshwar-kagatikar <siddeshwar2004@gmail.com>
2025-11-30 00:28:31 +0530

    emrgency automated

---
+++ mlruns/1/models/m-d4010aeae4e945279ffc51e3f4ebfc9c/artifacts/code/codes/register_model.py
@@ -0,0 +1,35 @@
+import mlflow
+import mlflow.sklearn
+import joblib
+import os
+import sys
+
+# Path to this script
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Add custom modules (pipeline.py, data_loader.py, etc.)
+sys.path.append(SCRIPT_DIR)
+
+# Model path
+MODEL_PATH = os.path.join(SCRIPT_DIR, "../models/model.pkl")
+
+def register_model():
+
+    model = joblib.load(MODEL_PATH)
+
+    mlflow.set_tracking_uri("http://127.0.0.1:5001")
+    mlflow.set_experiment("SPE_Model_Training")
+
+    with mlflow.start_run():
+
+        mlflow.sklearn.log_model(
+            sk_model=model,
+            artifact_path="model",               # MUST USE THIS
+            registered_model_name="SPE_Regression_Model",
+            code_paths=[SCRIPT_DIR]              # NOW MLflow will embed your code
+        )
+
+        print("Model successfully registered!")
+
+if __name__ == "__main__":
+    register_model()
